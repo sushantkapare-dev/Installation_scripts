4b69f53e5924f5c112934e8e1ab9909cc4c5ddd9
Sushant Kapare <sushantkapare1717@gmail.com>
2023-12-19 16:32:48 +0530

    Update Puppet.sh

--- Puppet.sh
+++ Puppet.sh
@@ -1,1 +1,15 @@
 
+#!/bin/bash
+
+# Add Puppet repository
+sudo apt-get update
+sudo apt-get install -y ca-certificates
+sudo wget https://apt.puppet.com/puppet7-release-bionic.deb
+sudo dpkg -i puppet7-release-bionic.deb
+sudo apt-get update
+
+# Install Puppet
+sudo apt-get install -y puppet-agent
+
+# Verify installation
+sudo /opt/puppetlabs/bin/puppet --version
