fbf33dae07ee8c2deacec90f294ce79495b10122
Sushant Kapare <sushantkapare1717@gmail.com>
2024-03-19 08:53:49 +0530

    Create script.sh

---
+++ Terraform/script.sh
@@ -0,0 +1,21 @@
+## INSTALL TERRAFORM ON UBUNTU
+
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common -y
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform -y
+
+## INSTALL TERRAFORM ON AMAZON LINUX
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+terraform --version
